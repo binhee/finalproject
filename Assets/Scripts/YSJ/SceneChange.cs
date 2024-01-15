a0c1748c2da1661d373b1f258745aaac1e22ef8a
YuSeonJae <985221@naver.com>
2024-01-15 20:48:00 +0900

    [Update]ButtonPopup
    Inventory,Store,Stage Button Popup Add

--- Assets/Scripts/YSJ/SceneChange.cs
+++ Assets/Scripts/YSJ/SceneChange.cs
@@ -5,14 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    // Start is called before the first frame update
     public void InventoryLoad()     // 인벤토리 로드 함수
     {
-        SceneManager.LoadScene("JHS_Item");     // "" 인벤토리 Scene 추가
+        GameObject go = Resources.Load<GameObject>("YSJ/Inventory");
+        Instantiate(go);
     }
 
     public void StoreLoad()     // 상점 로드 함수
     {
-        SceneManager.LoadScene("JHS_Item");     // "" 상점 Scene 추가
+        GameObject go = Resources.Load<GameObject>("YSJ/Store");
+        Instantiate(go);
+    }
+
+    public void Stage1Load()    // 스테이지1 입장
+    {
+        SceneManager.LoadScene("JBH_Stage");    // "" 스테이지 씬 삽입
+    }
+
+    public void Stage2Load()    // 스테이지2 입장
+    {
+        SceneManager.LoadScene("JBH_Stage");    // "" 스테이지 씬 삽입
+    }
+
+    public void Stage3Load()    // 스테이지3 입장
+    {
+        SceneManager.LoadScene("JBH_Stage");    // "" 스테이지 씬 삽입
     }
 }
+
+
