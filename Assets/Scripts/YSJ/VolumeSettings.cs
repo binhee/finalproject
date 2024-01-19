37bd7c0b84d16103fb2b805884ff42e8a19ba00c
YuSeonJae <985221@naver.com>
2024-01-19 19:55:31 +0900

    [Update]Sound

--- Assets/Scripts/YSJ/VolumeSettings.cs
+++ Assets/Scripts/YSJ/VolumeSettings.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
@@ -7,29 +9,15 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] private AudioMixer audioMixer;
     [SerializeField] private Slider BGMSlider;
 
-    //private void Start()
-    //{
-    //    if (PlayerPrefs.HasKey("BGMVolume"))
-    //    {
-    //        LoadVolume();
-    //    }
-    //    else
-    //    {
-    //        SetBGMVolume();     // 슬라이더 값과 오디오 믹서 호환.
-    //    }        
-    //}
+    private void Start()
+    {
+        SetBGMVolume();     // 슬라이더 값과 오디오 믹서 호환.          
+    }
+    
     public void SetBGMVolume()
     {
         float volume = BGMSlider.value;
         audioMixer.SetFloat("BGM", Mathf.Log10(volume)*20);
-        //PlayerPrefs.SetFloat("BGMVolume", volume);  //BGMVolume 이름으로 슬라이더 값을 저장
     }
 
-    //private void LoadVolume()
-    //{
-    //    //슬라이더 볼륨을 BGMVolume 이름으로 저장한 값과 동일시 함.
-    //    BGMSlider.value = PlayerPrefs.GetFloat("BGMvolume");
-
-    //    SetBGMVolume();
-    //}
 }
