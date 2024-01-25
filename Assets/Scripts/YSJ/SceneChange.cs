a23a7aa487aacb936591fdfd9e32dc28191e3c88
YuSeonJae <985221@naver.com>
2024-01-25 21:13:01 +0900

    [Bug]Sound

--- Assets/Scripts/YSJ/SceneChange.cs
+++ Assets/Scripts/YSJ/SceneChange.cs
@@ -42,22 +42,24 @@ public class SceneChange : MonoBehaviour
         soundManager.PlatSFX(soundManager.BTNSound);    // BTNSound 재생
         bool IsSetOptionPanel = OptionPanel.activeSelf;
         OptionPanel.SetActive(!IsSetOptionPanel);
-        //if (!IsSetOptionPanel)
-        //{
-        //    // 판넬이 나타날 때 일시정지
-        //    Time.timeScale = 0f;
-        //}
-        //else
-        //{
-        //    // 판넬이 사라질 때 일시정지 해제
-        //    Time.timeScale = 1f;
-        //}
-        //Time.fixedDeltaTime = 0.02f*Time.timeScale;
+        if (!IsSetOptionPanel)
+        {
+            // 판넬이 나타날 때 일시정지
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            // 판넬이 사라질 때 일시정지 해제
+            Time.timeScale = 1f;
+        }
+        Time.fixedDeltaTime = 0.02f * Time.timeScale;
     }
 
     public void MainSceneLoad()    // 메인화면 입장
     {
         SceneManager.LoadScene("Main");    // "" 스테이지 씬 삽입
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = 0.02f * Time.timeScale;
     }
 
     public void Stage1Load()    // 스테이지1 입장
