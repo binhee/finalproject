285f75ba5373fdecd1964901d4e3cc23e2f6a370
binhee <bh7149@naver.com>
2024-01-26 20:50:37 +0900

    [Update] Boss Map
    스테이지 및 보스맵 수정

--- Assets/Scripts/JBH/Minimap/Teleport.cs
+++ Assets/Scripts/JBH/Minimap/Teleport.cs
@@ -33,7 +33,7 @@ public class Teleport : MonoBehaviour
     IEnumerator TeleportRoutine()
     {
         Boss.SetActive(true);
-        mainCamera.transform.position += new Vector3(50, 0, 0);
+        mainCamera.transform.position += new Vector3(100, 0, 0);
         Debug.Log("h");
         yield return null;   // 한 프레임 대기
         targetObj.transform.position = toObj.transform.position;   // 대상 오브젝트를 목표 위치로 이동
