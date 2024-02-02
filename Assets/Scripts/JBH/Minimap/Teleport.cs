2a22d5499c95634e2f980753a1f14e40644261e8
binhee <bh7149@naver.com>
2024-02-02 12:50:14 +0900

    [Update] Stage2 Map Moving
    스테이지 1, 2 보스 입장 시 시네머신 카메라 움직임 수정, 스테이지2 맵 타일 이동 구현

--- Assets/Scripts/JBH/Minimap/Teleport.cs
+++ Assets/Scripts/JBH/Minimap/Teleport.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.Rendering.VirtualTexturing;
 
@@ -10,37 +11,41 @@ public class Teleport : MonoBehaviour
     public GameObject Boss;
     public GameObject mainCamera;
     public GameObject camera1;    // 시네머신 카메라
-    public GameObject camera2;    
+    public GameObject camera2;
+    public GameObject camera3;
 
     // 트리거 영역에 플레이어가 진입했을 때 호출되는 메서드
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            Boss.SetActive(true);
             targetObj = collision.gameObject;   // 대상 오브젝트를 플레이어로 설정
             StartCoroutine(TeleportRoutine());   // 텔레포트 루틴 시작
         }
     }
 
     // 트리거 영역에 플레이어가 머물러 있는 동안 계속 호출되는 메서드
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            //StartCoroutine(TeleportRoutine());   // 텔레포트 루틴 시작
-        }
-    }
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag("Player"))
+    //    {
+    //        //StartCoroutine(TeleportRoutine());   // 텔레포트 루틴 시작
+    //    }
+    //}
 
     // 텔레포트를 수행하는 코루틴
     IEnumerator TeleportRoutine()
     {
         camera1.SetActive(false);
-
-        Boss.SetActive(true);
-        mainCamera.transform.position += new Vector3(100, 0, 0);        
-        yield return null;   // 한 프레임 대기
-        targetObj.transform.position = toObj.transform.position;   // 대상 오브젝트를 목표 위치로 이동        
         
-        camera2.SetActive(true);        
+        //mainCamera.transform.position += new Vector3(100, 0, 0);
+        yield return null;   // 한 프레임 대기
+        targetObj.transform.position = toObj.transform.position;   // 대상 오브젝트를 목표 위치로 이동
+
+        camera3.SetActive(true);
+        yield return null;
+        camera3.SetActive(false);
+        camera2.SetActive(true);
     }
 }
