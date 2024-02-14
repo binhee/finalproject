5e7b8da3f2540d1949bdfcf4b14004f2aec55326
joduhee <engml9252@gmail.com>
2024-02-14 17:35:56 +0900

    [Update] boss3 logic
    보스 3패턴과 에셋, 체력바 적용

---
+++ Assets/Scripts/JDH/JDH_Scripts/JDH_Boss3/Boss3.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Boss3State { Pattern01, Pattern02 };
+
+public class Boss3 : MonoBehaviour
+{
+    private Boss3State boss3State = Boss3State.Pattern01;
+    [SerializeField]
+    private StageData stageData;
+    
+    private Boss3HP boss3hp;
+    private Boss3Weapon boss3Weapon;
+    private Movement2D movement2D;
+    
+
+    private void Awake()
+    {
+        movement2D = GetComponent<Movement2D>();
+        boss3hp = GetComponent<Boss3HP>();
+        boss3Weapon = GetComponent<Boss3Weapon>();
+    }
+
+    public void ChangePattern(Boss3State newstate)
+    {
+        StopCoroutine(boss3State.ToString());
+        boss3State = newstate;
+        StartCoroutine(boss3State.ToString());
+    }
+
+    private IEnumerator Patteron01()
+    {
+        boss3Weapon.StartAttack(Boss3AttackType.SingleToPlayer);
+        while(true)
+        {
+            if(boss3hp.CurrentHP3<=boss3hp.MaxHP3)
+            {
+                boss3Weapon.StopAttack(Boss3AttackType.SingleToPlayer);
+                ChangePattern(Boss3State.Pattern02);
+            }
+            yield return null;
+        }
+    }
+
+    private IEnumerator Patteron02()
+    {
+        boss3Weapon.StartAttack(Boss3AttackType.CircleForPlayer);
+        while (true)
+        {
+            if(boss3hp.CurrentHP3<=boss3hp.MaxHP3)
+            {
+                boss3Weapon.StopAttack(Boss3AttackType.CircleForPlayer);
+                
+            }
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/JDH/JDH_Scripts/JDH_Boss3/Boss3Die.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss3Die : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject explosionDie;
+    [SerializeField]
+    private GameObject ClearPanel;
+
+    [SerializeField]
+    private int Boss3Gold = 3000;
+
+    private PlayerController playerController;
+
+    private void Awake()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
+    public void Boss2OnDie()
+    {
+        Instantiate(explosionDie, transform.position, Quaternion.identity);
+        ClearPanel.SetActive(true);
+        gameObject.SetActive(false);
+        PlayerManager.instance.playerGold += Boss3Gold;
+
+        PlayerPrefs.SetInt("Gold", PlayerManager.instance.playerGold);
+
+    }
+
+}
