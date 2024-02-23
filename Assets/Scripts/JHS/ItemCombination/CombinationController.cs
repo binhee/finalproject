ea664239766d63569ab2aa9a4f247b22b9b3b65b
NLJKAM <nljbero540@gmail.com>
2024-02-23 14:12:51 +0900

    [Fix] combination

--- Assets/Scripts/JHS/ItemCombination/CombinationController.cs
+++ Assets/Scripts/JHS/ItemCombination/CombinationController.cs
@@ -13,23 +13,26 @@ public class CombinationController : MonoBehaviour
 
     public void Combination()
     {
-        if (baseSlot.transform.childCount == 1 && ingredientSlot.transform.childCount == 1)
+        if (resultSlot.transform.childCount == 0)
         {
-            if (baseSlot.transform.GetChild(0).GetComponent<BaseItem>() != null &&
-              ingredientSlot.transform.GetChild(0).GetComponent<IngredientItem>() != null)
+            if (baseSlot.transform.childCount == 1 && ingredientSlot.transform.childCount == 1)
             {
-                if (baseSlot.transform.GetChild(0).GetComponent<BaseItem>().baseItem == true &&
-                            ingredientSlot.transform.GetChild(0).GetComponent<IngredientItem>().baseItem == false)
+                if (baseSlot.transform.GetChild(0).GetComponent<BaseItem>() != null &&
+                  ingredientSlot.transform.GetChild(0).GetComponent<IngredientItem>() != null)
                 {
-                    resultItem = baseSlot.transform.GetChild(0).GetComponent<BaseItem>().Combine(ingredientSlot.transform.GetChild(0).gameObject);
-                    Instantiate(resultItem, resultSlot.transform);
-                    Destroy(baseSlot.transform.GetChild(0).gameObject);
-                    Destroy(ingredientSlot.transform.GetChild(0).gameObject);
+                    if (baseSlot.transform.GetChild(0).GetComponent<BaseItem>().baseItem == true &&
+                                ingredientSlot.transform.GetChild(0).GetComponent<IngredientItem>().baseItem == false)
+                    {
+                        resultItem = baseSlot.transform.GetChild(0).GetComponent<BaseItem>().Combine(ingredientSlot.transform.GetChild(0).gameObject);
+                        Instantiate(resultItem, resultSlot.transform);
+                        Destroy(baseSlot.transform.GetChild(0).gameObject);
+                        Destroy(ingredientSlot.transform.GetChild(0).gameObject);
+                    }
                 }
-            }
-            else
-            {
+                else
+                {
 
+                }
             }
         }
     }
