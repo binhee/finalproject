757d5e2a819d8a5cea1408068216828508ca7e1c
NLJKAM <nljbero540@gmail.com>
2024-02-24 21:49:45 +0900

    [Fix] EnforcePercent
    강화확률 적용

--- Assets/Scripts/JHS/ItemEnforce/ItemEnforce.cs
+++ Assets/Scripts/JHS/ItemEnforce/ItemEnforce.cs
@@ -15,6 +15,8 @@ public class ItemEnforce : MonoBehaviour
     public TextMeshProUGUI enforceItemTxt;
     public TextMeshProUGUI enforceResultTxt;
 
+    int cost;
+    float percent;
     void Update()
     {
         Enforce();
@@ -25,7 +27,7 @@ public class ItemEnforce : MonoBehaviour
         {
             GameObject enforceItem = enforceSlot.transform.GetChild(0).gameObject;
             ClassifyItemGrade(enforceItem);
-            if(grade == 3)
+            if (grade == 3)
             {
                 MaxEnforceItemByGrade();
             }
@@ -43,7 +45,7 @@ public class ItemEnforce : MonoBehaviour
     void ClassifyItemGrade(GameObject item)
     {
         DraggableUI dragitem = item.GetComponent<DraggableUI>();
-        if(dragitem.itemImageType==ItemType.HpPotion|| dragitem.itemImageType == ItemType.SpeedPotion||
+        if (dragitem.itemImageType == ItemType.HpPotion || dragitem.itemImageType == ItemType.SpeedPotion ||
             dragitem.itemImageType == ItemType.EnchantPotion || dragitem.itemImageType == ItemType.JumpPotion)
         {
             grade = item.GetComponent<PotionAction>().grade;
@@ -52,7 +54,7 @@ public class ItemEnforce : MonoBehaviour
         {
             grade = item.GetComponent<WeaponAction>().grade;
         }
-        else if (dragitem.itemImageType == ItemType.Helmet|| dragitem.itemImageType == ItemType.Armor
+        else if (dragitem.itemImageType == ItemType.Helmet || dragitem.itemImageType == ItemType.Armor
             || dragitem.itemImageType == ItemType.Boots)
         {
             grade = item.GetComponent<ArmorAction>().grade;
@@ -60,31 +62,39 @@ public class ItemEnforce : MonoBehaviour
     }
     public void EnforceButton()
     {
-        if (enforceSlot.transform.childCount == 1&& enforceSlot.transform.GetChild(0).gameObject.GetComponent<ItemAction>().grade < 3)
+        if (enforceSlot.transform.childCount == 1 && enforceSlot.transform.GetChild(0).gameObject.GetComponent<ItemAction>().grade < 3)
         {
-            GameObject enforceItem = enforceSlot.transform.GetChild(0).gameObject;
-            DraggableUI dragitem = enforceItem.GetComponent<DraggableUI>();
-            if (dragitem.itemImageType == ItemType.HpPotion || dragitem.itemImageType == ItemType.SpeedPotion ||
-            dragitem.itemImageType == ItemType.EnchantPotion || dragitem.itemImageType == ItemType.JumpPotion)
+            if (PlayerManager.instance.playerGold >= cost)
             {
-                enforceItem.GetComponent<PotionAction>().grade++;
-            }
-            else if (dragitem.itemImageType == ItemType.Weapon)
-            {
-                enforceItem.GetComponent<WeaponAction>().grade++;
-            }
-            else if (dragitem.itemImageType == ItemType.Helmet || dragitem.itemImageType == ItemType.Armor
-                || dragitem.itemImageType == ItemType.Boots)
-            {
-                enforceItem.GetComponent<ArmorAction>().grade++;
-            }
+                PlayerManager.instance.playerGold -= cost;
+                if (Random.RandomRange(0, 100) > percent)
+                {
+                    return;
+                }
 
+                GameObject enforceItem = enforceSlot.transform.GetChild(0).gameObject;
+                DraggableUI dragitem = enforceItem.GetComponent<DraggableUI>();
+                if (dragitem.itemImageType == ItemType.HpPotion || dragitem.itemImageType == ItemType.SpeedPotion ||
+                dragitem.itemImageType == ItemType.EnchantPotion || dragitem.itemImageType == ItemType.JumpPotion)
+                {
+                    enforceItem.GetComponent<PotionAction>().grade++;
+                }
+                else if (dragitem.itemImageType == ItemType.Weapon)
+                {
+                    enforceItem.GetComponent<WeaponAction>().grade++;
+                }
+                else if (dragitem.itemImageType == ItemType.Helmet || dragitem.itemImageType == ItemType.Armor
+                    || dragitem.itemImageType == ItemType.Boots)
+                {
+                    enforceItem.GetComponent<ArmorAction>().grade++;
+                }
+            }
         }
     }
     void EnforceItemByGrade()
     {
-        float percent = 100 / (grade + 1);
-        int cost = (grade + 1) * 200;
+        percent = 100 / (grade + 1);
+        cost = (grade + 1) * 200;
         enforceItemTxt.text = $"강화 단계 {grade} -> {grade + 1}\n강화 확률 {percent} %\n소모골드 {cost}";
     }
     void MaxEnforceItemByGrade()
@@ -94,6 +104,6 @@ public class ItemEnforce : MonoBehaviour
     void ResetEnforceItemText()
     {
         enforceNameTxt.text = "없음";
-        enforceItemTxt.text = $"강화 단계 {0} -> {0+ 1}\n강화 확률 {0} %\n소모골드 {0}";
+        enforceItemTxt.text = $"강화 단계 {0} -> {0 + 1}\n강화 확률 {0} %\n소모골드 {0}";
     }
 }
