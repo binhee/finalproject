d053618b48a39b6acf4912bd07ad2ebc28ca8b67
joduhee <engml9252@gmail.com>
2024-02-26 11:32:09 +0900

    using 제거

--- Assets/Scripts/JBH/Minimap/Teleport.cs
+++ Assets/Scripts/JBH/Minimap/Teleport.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
-using UnityEngine.Rendering.VirtualTexturing;
+
 
 public class Teleport : MonoBehaviour
 {
